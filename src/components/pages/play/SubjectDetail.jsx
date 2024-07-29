f1702e39ac599fd8df54f96b91bd450326c64fbd
thesandfox <sandfox9411@gmail.com>
2024-07-29 17:33:50 +0900

    240729

--- src/components/pages/play/SubjectDetail.jsx
+++ src/components/pages/play/SubjectDetail.jsx
@@ -2,7 +2,10 @@ import { Suspense, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { SUBJECTS } from '/src/datas/subjects';
 import { SubjectScene } from './SubjectScene';
+import { FaChevronLeft } from "react-icons/fa6";
 import React from 'react';
+import './subjectdetail.css';
+import { InputText } from '../../generic/Input';
 
 export function SubjectDetail({subjectId}) {
 	// const { subjectId } = useParams();
@@ -49,13 +52,55 @@ export function SubjectDetail({subjectId}) {
 		console.log(subjectState);
 	},[subjectState])
 	//리턴JSX
-	return <>
-		<SubjectScene>
-			{}
-			{controllerJSX}
-			<Suspense fallback={null}>
-				{sceneJSX}
-			</Suspense>
-		</SubjectScene>
-	</>
+	return <div className='subjectDetail'>
+		{/* 씬 래퍼 */}
+		<div className='subjectSceneWrapper'>
+			{/* 씬 */}
+			<SubjectScene>
+				{}
+				{controllerJSX}
+				<Suspense fallback={null}>
+					{sceneJSX}
+				</Suspense>
+			</SubjectScene>
+			{/* 씬헤더 */}
+			<div className='header'>
+				<div className='actName font_main'>{}</div>
+				<div className='subjectName font_medium'>{}</div>
+				<div className='goBack'>
+					<FaChevronLeft className='icon'/>
+				</div>
+			</div>
+		</div>
+		{/* 메인영역 */}
+		<div className='mainContext'>
+			{/* 상단-지문 */}
+			<div className='zimoonWrapper'>
+				<div className='zimoon'>
+					<div className='subjectName font_main'>
+						{'단원이름 + 문제이름'}
+					</div>
+					<div className='zimoonContent font_medium'>
+						{'문제의 지문이다'}
+					</div>
+				</div>
+			</div>
+			{/* 중단-답변 */}
+			<div className='answerInput'>
+				<div className='answerTitle font_main'>정답 입력</div>
+				<div className='inputs'>
+					<div className='labelAndInput'>
+						<div className='label'>
+							정답1
+						</div>
+						<InputText/>
+					</div>
+				</div>
+			</div>
+			{/* 하단-버튼들 */}
+			<div className='bottomContext'>
+
+			</div>
+		</div>
+	</div>
 }
