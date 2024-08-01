e0961490f9fabed9d2bafd9522a1af3c8a637485
thesandfox <sandfox9411@gmail.com>
2024-08-01 15:26:29 +0900

    240801

---
+++ src/datas/subjects/Subject3_1.jsx
@@ -0,0 +1,69 @@
+import { useEffect,useMemo,Fragment } from "react";
+import { Html, Line } from '@react-three/drei';
+import '/src/components/generic/subject/subject.css';
+
+function Adjust({subjectState,answer,handleCorrect}) {
+	useEffect(()=>{
+		if (!answer) {return;}
+		if (!handleCorrect) {return;}
+		handleCorrect.set(String(answer)===String(subjectState.vertexes.length))
+	},[answer,handleCorrect]);
+	return <>
+	</>
+}
+
+// Scene
+function Scene({subjectState}) {
+	const {vertexes} = subjectState;
+	return <group>
+		{
+			// 선
+			vertexes
+			?<Line
+				points={[...vertexes]} // 라인의 점들
+				color="#1A3659" // 라인의 색상
+				lineWidth={2} // 라인의 두께
+		  	/>
+			:<></>
+		}
+		{
+			// 점두개
+			vertexes
+			?vertexes.map((vertex,index)=>{
+				return <Fragment key={index}>
+					<Html position={vertex}>
+						<div className="subjectVertex">
+
+						</div>
+					</Html>
+				</Fragment>
+			})
+			:<></>
+		}
+	</group>
+}
+
+// Controller
+function Controller({handleSubjectState}) {
+	const vertexes = useMemo(()=>{
+		return [
+			[-2,0,2],
+			[2,0,2],
+			[2,0,-2],
+			[-2,0,-2],
+		]
+	},[])
+	//핸들러 작동
+	useEffect(()=>{
+		// console.log('핸들러작동');
+		handleSubjectState.set({
+			vertexes
+		});
+	},[vertexes]);
+}
+
+export {
+	Adjust,
+	Scene,
+	Controller
+}
