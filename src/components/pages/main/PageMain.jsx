8060e21eacec47e9fd8606aad7b3e6e53ae262f0
Sauser <gubonil13@naver.com>
2024-08-07 15:25:43 +0900

    mainHeader추가

--- src/components/pages/main/PageMain.jsx
+++ src/components/pages/main/PageMain.jsx
@@ -1,16 +1,17 @@
 import Navigation from "../navigation/navigation";
-import { Header } from "./sections/header/header";
+// import { Header } from "./sections/header/header";
 import { Section1 } from "./sections/sec1/section1";
 import { Section2 } from "./sections/sec2/section2";
 import { Section3 } from "./sections/sec3/section3";
 import { Section4 } from "./sections/sec4/section4";
 import { Footer } from "./sections/footer/footer";
+import { MainHeader } from "../header/MainHeader"
 
 export function PageMain({}) {
   return (
     <>
       <Navigation />
-      {/* <Header /> */}
+      <MainHeader/>
       <Section1 />
       <Section2 />
       <Section3 />
